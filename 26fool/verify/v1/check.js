59ddca152725b1dd3218e0f6ae8d20131789b41a
GZXUEXUE <yuguanxue1024@foxmail.com>
2025-09-24 14:29:31 +0800

    Add files via upload

---
+++ 26fool/verify/v1/check.js
@@ -0,0 +1,6 @@
+var ans = "Etcxg Ycxg";
+function verify(){
+    inp = document.WhyAreYouLookingThis.veri.value;
+    if (inp == ans) document.WhyAreYouLookingThis.result.value = "Verification successful! The password is: qwfa34eiji3 .";
+    else document.WhyAreYouLookingThis.result.value = "Verification failed.";
+}
