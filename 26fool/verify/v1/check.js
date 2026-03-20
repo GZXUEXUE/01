8923daefbd7bcd56c7ccbf036cfb943e8f7ce302
GZXUEXUE <yuguanxue1024@foxmail.com>
2026-03-20 13:47:09 +0800

    Update verification answer and success message

--- 26fool/verify/v1/check.js
+++ 26fool/verify/v1/check.js
@@ -1,7 +1,7 @@
-var ans = "Etcxg Ycxg";
+var ans = "ginopelia";
 function verify(){
     inp = document.WhyAreYouLookingThis.veri.value;
     var res_ele = document.getElementsByName("result")[0];
-    if (inp == ans) res_ele.innerHTML = "Verification successful! The password is: qwfa34eiji3 .";
+    if (inp == ans) res_ele.innerHTML = "Verification successful! The password is: terminal";
     else res_ele.innerHTML = "Verification failed.";
 }
